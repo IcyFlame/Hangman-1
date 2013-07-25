c9c2c3f344b6d466cedfba609d19efd7bbb3327d
Siddharth Kannan <kannan.siddharth12@gmail.com>
2013-07-25 13:15:29 +0530

    changed file Hang.js so that once a letter is guessed then that button will become inactive. Ensuring that the user can't press that button again.

--- js/Hang.js
+++ js/Hang.js
@@ -72,6 +72,9 @@ $(document).ready(function () {
 
     $(".inputBtn").click(function() {
         j = 0;
+
+	this.setAttribute("disabled","disabled");
+	
         hideIt(this.value);
         setValue(this.value, length);
         changeImage("#img");
